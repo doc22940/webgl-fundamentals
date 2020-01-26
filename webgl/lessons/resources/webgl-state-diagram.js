e316aed6ed64c5d92d05deae18443e8304536009
Gregg Tavares <github@greggman.com>
2020-01-26 22:50:35 +0900

    wip

--- webgl/lessons/resources/webgl-state-diagram.js
+++ webgl/lessons/resources/webgl-state-diagram.js
@@ -5,9 +5,26 @@
 // * connect arrows
 // * position things
 // * texture mips
+
 var gl;
 
 function main() {
+  const {
+    px,
+    wait,
+    formatG,
+    formatX2,
+    formatEnum,
+    formatEnumZero,
+    formatBoolean,
+    formatUniformValue,
+    createTemplate,
+    updateElem,
+    helpToMarkdown,
+    addElem,
+    createTable,
+  } = window.stateDiagramUtils;
+
   hljs.initHighlightingOnLoad();
 
   gl = document.querySelector('canvas').getContext('webgl');
@@ -17,19 +34,6 @@ function main() {
   const codeElem = document.querySelector('#code');
 
   const webglObjects = new Map();
-
-  const px = (v) => `${v}px`;
-  const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-  const formatG = v => typeof v === 'number' ? v.toFixed(3).replace(/\.?0+$/, '') : v;
-  function formatX(v, n = 0) {
-    const s = v.toString(16);
-    return `0x${s.padStart(n, '0').substr(0, 2)}`;
-  }
-  const formatX2 = v => formatX(v, 2);
-  const formatEnum = v => glEnumToString(gl, v);
-  const formatEnumZero = v => v ? v === 1 ? 'ONE' : glEnumToString(gl, v) : 'ZERO';
-  const formatBoolean = v => v.toString();
   const formatWebGLObject = v => v ? webglObjects.get(v).name : 'null';
   const formatWebGLObjectOrDefaultVAO = v => v ? webglObjects.get(v).name : 'null (default VAO)';
 
@@ -40,30 +44,6 @@ function main() {
     }, 1);
   }
 
-  function updateElem(elem, newValue) {
-    if (elem.textContent !== newValue) {
-      elem.textContent = newValue;
-      flash(elem);
-    }
-  }
-
-  function helpToMarkdown(s) {
-    s = s.replace(/---/g, '```')
-         .replace(/--/g, '`');
-    const m = /^\n( +)/.exec(s);
-    if (!m) {
-      return s;
-    }
-    const lines = s.split('\n');
-    if (lines[0].trim() === '') {
-      lines.shift();
-    }
-    if (lines[lines.length - 1].trim() === '') {
-      lines.pop();
-    }
-    const indent = m[1];
-    return lines.map(line => line.startsWith(indent) ? line.substr(indent.length) : line).join('\n');
-  }
 
   const converter = new showdown.Converter();
   const hintElem = document.querySelector('#hint');
@@ -81,7 +61,6 @@ function main() {
     hintElem.style.top = px(e.pageY + 5);
     hintElem.style.display = hint ? '' : 'none';
   }
-
   document.body.addEventListener('mousemove', function(e) {
     let elem = e.target;
     while (!elem.dataset.help && elem.nodeName !== 'BODY') {
@@ -158,27 +137,6 @@ function main() {
       `,
     },
   ];
-
-  function addElem(tag, parent, attrs = {}) {
-    const elem = document.createElement(tag);
-    for (const [key, value] of Object.entries(attrs)) {
-      if (typeof value === 'object') {
-        for (const [k, v] of Object.entries(value)) {
-          try {
-          elem[key][k] = v;
-          } catch (e) {
-            debugger;  // eslint-disable-line no-debugger
-          }
-        }
-      } else if (elem[key] === undefined) {
-        elem.setAttribute(key, value);
-      } else {
-        elem[key] = value;
-      }
-    }
-    parent.appendChild(elem);
-    return elem;
-  }
 
   let dragTarget;
   let dragMouseStartX;
@@ -263,16 +221,6 @@ function main() {
     titleElem.addEventListener('click', toggleExpander);
     titleElem.addEventListener('mousedown', (e) => e.stopPropagation());
     return outer;
-  }
-
-  function glEnumToString(gl, value) {
-    const keys = [];
-    for (const key in gl) {
-      if (gl[key] === value) {
-        keys.push(key);
-      }
-    }
-    return keys.length ? keys.join(' | ') : `0x${value.toString(16)}`;
   }
 
   function createStateTable(states, parent, title, queryFn, update = true) {
@@ -494,37 +442,6 @@ function main() {
 
   }());
 
-  function createTable(parent, headings) {
-    const table = addElem('table', parent);
-    const thead = addElem('thead', table);
-    headings.forEach(heading => addElem('th', thead, {textContent: heading}));
-    return addElem('tbody', table);
-  }
-
-  function formatUniformValue(v) {
-    if (v.buffer && v.buffer instanceof ArrayBuffer) {
-      v = Array.from(v);
-    }
-    if (Array.isArray(v)) {
-      if (v.length > 4) {
-        // should really look at type of uniform
-        const mod = v.length % 3 === 0 ? 3 : 4;
-        const rows = [];
-        for (let i = 0; i < v.length; i += mod) {
-          const row = [];
-          const end = Math.min(i + mod, v.length);
-          for (let j = i; j < end; ++j) {
-            row.push(formatG(v[j]));
-          }
-          rows.push(row.join(', '));
-        }
-        return rows.join(',\n');
-      }
-      return v.map(formatG).join(', ');
-    }
-    return typeof v === 'number' ? formatG(v) : v;
-  }
-
   function createProgramUniforms(parent, gl, program) {
     const tbody = createTable(parent, ['name', 'value']);
 
@@ -574,7 +491,7 @@ function main() {
       elem: tbody,
       scan,
       update,
-    }
+    };
   }
 
   function setName(elem, name) {
@@ -711,17 +628,6 @@ function main() {
       scanUniforms: uniformUI.scan,
       updateUniforms: uniformUI.update,
     };
-  }
-
-  function createTemplate(parent, selector) {
-    const template = document.querySelector(selector);
-    const collection = template.content.cloneNode(true);
-    if (collection.children.length !== 1) {
-      throw new Error('template must have 1 child');
-    }
-    const elem = collection.children[0];
-    parent.appendChild(elem);
-    return elem;
   }
 
   const maxAttribs = 8;
